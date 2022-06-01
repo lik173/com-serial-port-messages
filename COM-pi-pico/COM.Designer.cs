6fe5c5d8dbc3ac146ba0049034b63adf0e225ad3
lik173 <70771696+lik173@users.noreply.github.com>
2022-06-01 21:42:41 +0300

    Add files via upload

---
+++ COM-pi-pico/COM.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace COM_pi_pico
+{
+    partial class COM
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.kill_child = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(227, 9);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(564, 432);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "COM1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "COM2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(212, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "COM3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 171);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(212, 47);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "COM4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 224);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(212, 47);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "COM5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // kill_child
+            // 
+            this.kill_child.Location = new System.Drawing.Point(12, 383);
+            this.kill_child.Name = "kill_child";
+            this.kill_child.Size = new System.Drawing.Size(212, 47);
+            this.kill_child.TabIndex = 7;
+            this.kill_child.Text = "Destroy connection";
+            this.kill_child.UseVisualStyleBackColor = true;
+            this.kill_child.Click += new System.EventHandler(this.kill_child_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kill_child);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "COM Serial Messages";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button kill_child;
+    }
+}
+
---
+++ COM-pi-pico/COM.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace COM_pi_pico
+{
+    public partial class COM : Form
+    {
+        public COM()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+
+
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void kill_child_Click(object sender, EventArgs e)
+        {
+
+            string strCmdText;
+            strCmdText = "/C taskkill /f /im putty.exe";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Process p = Process.Start("notepad.exe");
+            var p = new Process();
+            p.StartInfo.FileName = "putty.exe";
+            p.StartInfo.Arguments = "-serial COM1";
+            p.Start();
+            Thread.Sleep(500); // Allow the process to open it's window
+            SetParent(p.MainWindowHandle, panel1.Handle);
+            //Change the docked windows size to match its parent's size. 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Process p = Process.Start("notepad.exe");
+            var p = new Process();
+            p.StartInfo.FileName = "putty.exe";
+            p.StartInfo.Arguments = "-serial COM2";
+            p.Start();
+            Thread.Sleep(500); // Allow the process to open it's window
+            SetParent(p.MainWindowHandle, panel1.Handle);
+            //Change the docked windows size to match its parent's size. 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Process p = Process.Start("notepad.exe");
+            var p = new Process();
+            p.StartInfo.FileName = "putty.exe";
+            p.StartInfo.Arguments = "-serial COM3";
+            p.Start();
+            Thread.Sleep(500); // Allow the process to open it's window
+            SetParent(p.MainWindowHandle, panel1.Handle);
+            //Change the docked windows size to match its parent's size. 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Process p = Process.Start("notepad.exe");
+            var p = new Process();
+            p.StartInfo.FileName = "putty.exe";
+            p.StartInfo.Arguments = "-serial COM4";
+            p.Start();
+            Thread.Sleep(500); // Allow the process to open it's window
+            SetParent(p.MainWindowHandle, panel1.Handle);
+            //Change the docked windows size to match its parent's size. 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Process p = Process.Start("notepad.exe");
+            var p = new Process();
+            p.StartInfo.FileName = "putty.exe";
+            p.StartInfo.Arguments = "-serial COM5";
+            p.Start();
+            Thread.Sleep(500); // Allow the process to open it's window
+            SetParent(p.MainWindowHandle, panel1.Handle);
+            //Change the docked windows size to match its parent's size. 
+        }
+    }
+}
